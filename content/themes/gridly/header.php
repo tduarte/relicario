6d1ec23581581beb8ba6e0ae1436f0870aa63d95
Thiago Duarte <eu@thiagoduarte.com.br>
2013-10-20 03:07:16 -0200

    Coloca a descrição do site no título, corrige endereço do fallback e adiciona informações de OG.

--- content/themes/gridly/header.php
+++ content/themes/gridly/header.php
@@ -1,27 +1,23 @@
 <!DOCTYPE html>
-<!-- Gridly WordPress Theme by Eleven Themes (http://www.eleventhemes.com) - Proudly powered by WordPress (http://wordpress.org) -->
-
-	<!-- meta -->
-		<html <?php language_attributes();?>> 
+<html <?php language_attributes();?>> 
 	<meta charset="<?php bloginfo('charset'); ?>" />
-	<title><?php bloginfo('sitename'); ?> <?php wp_title(); ?></title>
+	<title><?php bloginfo('sitename'); ?> <?php wp_title(); ?> - <?php bloginfo('description'); ?></title>
 	<meta name="description" content="<?php bloginfo('description'); ?>"/>
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-		<?php  $options = get_option('plugin_options');
-			$gridly_color_scheme = $options['gridly_color_scheme'];
-			$gridly_logo = $options['gridly_logo'];
-			$gridly_responsive = $options['gridly_responsive'];?> 
-		
-		<?php if ($gridly_responsive != 'no') { ?>
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-		<?php  } ?>
-		
-	<!-- wp head -->
-	<?php wp_head(); ?>
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	
+		<?php wp_head(); ?>
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
-	
+		<meta property="og:title" content="<?php the_title(); ?>"/>
+		<meta property="og:description" content="<?php bloginfo('description'); ?>"/>
+		<meta property="og:url" content="<?php the_permalink(); ?>"/>
+		<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/og_facebook.png"/>
+		<meta property="og:type" content="<?php
+		  if (is_single() || is_page()) { echo "article"; } else { echo "website";}
+		?>"/>
+		<meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
 </head>
 
 <body <?php body_class(); ?>>
@@ -32,7 +28,7 @@
 			<div id="logo">
 					<a href="<?php echo home_url( '/' ); ?>"  title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 						<object data="<?php echo get_template_directory_uri(); ?>/images/logo.svg" type="image/svg+xml">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/light/logo.png" alt="<?php bloginfo('sitename'); ?>" />
+							<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('sitename'); ?>" />
 						</object>
 					</a>			
 			</div>
